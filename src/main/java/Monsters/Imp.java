13c98b6647cbd9c0df8e17e05d6ce9d32ef01d00
dracela <doracela@csumb.edu>
2024-07-30 00:02:38 -0700

    updated both attack classes, imp, and kobold

--- src/main/java/Monsters/Imp.java
+++ src/main/java/Monsters/Imp.java
@@ -2,6 +2,8 @@ package Monsters;
 
 import java.util.HashMap;
 
+import Abilities.MeleeAttack;
+
 /**
  * @author Dominick Racela
  */
@@ -9,6 +11,14 @@ import java.util.HashMap;
 public class Imp extends Monster {
     public Imp(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
         super(maxHP, xp, items);
+        Integer maxStr = 15;
+        Integer maxDef = 6;
+        Integer maxAgi = 3;
+
+        attack = new MeleeAttack(this);
+        str = super.getAttribute(str, maxStr);
+        def = super.getAttribute(def, maxDef);
+        agi = super.getAttribute(agi, maxAgi);
     }
 
     @Override
--- src/main/java/Monsters/Kobold.java
+++ src/main/java/Monsters/Kobold.java
@@ -2,6 +2,8 @@ package Monsters;
 
 import java.util.HashMap;
 
+import Abilities.MeleeAttack;
+
 /**
  * @author Dominick Racela
  */
@@ -9,6 +11,11 @@ import java.util.HashMap;
 public class Kobold extends Monster {
     public Kobold(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
         super(maxHP, xp, items);
+
+        attack = new MeleeAttack(this);
+        str = super.getAttribute(str, maxStr);
+        def = super.getAttribute(def, maxDef);
+        agi = super.getAttribute(agi, maxAgi);
     }
 
     @Override
